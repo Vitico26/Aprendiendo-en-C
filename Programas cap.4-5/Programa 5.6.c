ab3979409f63e8171299f4f3c934949a0beeeaf4
Vitico26 <victorengelscuevas@gmail.com>
2023-06-09 06:29:07 -0400

    Continuaion programas 4-5

--- Programas cap.4-5/Programa 5.6.c
+++ Programas cap.4-5/Programa 5.6.c
@@ -1,48 +1,48 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<math.h>
 
-/*Suma - cuadrados.
+/* Suma-cuadrados.
 El programa calcula la suma del cuadrado de los elementos de un arreglo
-unidimensional de 100 elementos de tipo real. */
+➥unidimensional de 100 elementos de tipo real. */
 
 const int MAX = 100;
-/* MAX se utiliza para reservar el espacio maximo que podra ocupar el arreglo. */
+/* MAX se utiliza para reservar el espacio máximo que podrá ocupar el arreglo. */
 
-void Lectura (float *, int);  /*Prototipos de funciones. */
-double Suma (float *, int);
+void Lectura(float *, int);      /* Prototipos de funciones. */
+double Suma(float *, int);
 
-int main(void)
+void main(void)
 {
     float VEC[MAX];
     double RES;
-    Lectura (VEC, MAX);
-    RES = Suma (VEC, MAX);
-    /* Se llama a la funcion Suma y se almacena el resultado en la variable RES. */
-    printf("\n\nSuma del arreglo: %.2lf" , RES);
+    Lectura(VEC, MAX);
+    RES = Suma(VEC, MAX);
+    RES = Suma(VEC, MAX);
+/* Se llama a la función Suma y se almacena el resultado en la variable RES. */
+    printf("\n\nSuma del arreglo: %.2f", RES);
 }
 
-void Lectura (float A[], int T)
-/*La funcion Lectura se utiliza para leer un arreglo inidimensional de T
-elementos de tipo real. */
+void Lectura(float A[], int T)
+/* La función Lectura se utiliza para leer un arreglo unidimensional de T
+➥elementos de tipo real. */
 {
-int I;
-for (I=0; I<T; I++)
-{
-    printf("Ingrese el elemento %d: ", I+1);
-    scanf("%f", &A[I]);
-}
+    int I;
+    for(I = 0; I < T; I++)
+    {
+        printf("Ingrese el elemento %d: ", I + 1);
+        scanf("%f", &A[I]);
+    }
 }
 
-double Suma (float A [], int T)
-/*La funcion Suma se utiliza para calcular la suma del cuadrado de los
-componentes de un arreglo inidimensional de T elementos de tipo real. */
-
+double Suma(float A[], int T)
+/* La función Suma se utiliza para calcular la suma del cuadrado de los
+➥componentes de un arreglo unidimensional de T elementos de tipo real. */
 {
     int I;
     double AUX = 0.0;
-    for(I=0; I<T; I++)
-    AUX += pow(A[I], 2);
+    for(I = 0; I < T; I++)
+    {
+        AUX += pow(A[I], 2);
+    }
     return(AUX);
-
 }
-
